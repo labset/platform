6ebdd200d35cbb200d2f7f955481363f4410fc88
viqueen <hasnae@viqueen.org>
2023-12-31 11:45:39 +1100

    [noissue] export doc entity access

--- modules/backend/db/src/dynamodb-access/index.ts
+++ modules/backend/db/src/dynamodb-access/index.ts
@@ -1,6 +1,7 @@
 export * from './clients';
 export * from './create-table-migration';
 export * from './data';
+export * from './doc-entity-access';
 export * from './doc-entity-read-access';
 export * from './doc-entity-write-access';
 export * from './migration';
