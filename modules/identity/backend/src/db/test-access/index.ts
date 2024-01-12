3581f74521579299d07a185ebb1d9b320904d247
viqueen <hasnae@viqueen.org>
2024-01-12 19:59:40 +1100

    buu yarn format

--- modules/identity/backend/src/db/test-access/index.ts
+++ modules/identity/backend/src/db/test-access/index.ts
@@ -1,12 +1,13 @@
-import {IdentityDynamoDbClients} from "../dynamodb-access";
+import { IdentityDynamoDbClients } from '../dynamodb-access';
 
-const testIdentityDynamoDbClients = async (): Promise<IdentityDynamoDbClients> => {
-    const clients = new IdentityDynamoDbClients({
-        region: 'local',
-        endpoint: process.env.MOCK_DYNAMODB_ENDPOINT ?? 'oops'
-    })
-    await clients.upgrade();
-    return clients;
-}
+const testIdentityDynamoDbClients =
+    async (): Promise<IdentityDynamoDbClients> => {
+        const clients = new IdentityDynamoDbClients({
+            region: 'local',
+            endpoint: process.env.MOCK_DYNAMODB_ENDPOINT ?? 'oops'
+        });
+        await clients.upgrade();
+        return clients;
+    };
 
 export { testIdentityDynamoDbClients };
