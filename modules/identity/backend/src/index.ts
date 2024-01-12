d82b6e43bbf6bd884c6a11c797a1169958a29b17
viqueen <hasnae@viqueen.org>
2024-01-12 20:29:01 +1100

    welcome with passport middleware

---
+++ modules/identity/backend/src/index.ts
@@ -0,0 +1,3 @@
+export * from './db';
+export * from './middlewares';
+export * from './services';
---
+++ modules/identity/backend/src/middlewares/with-passport-auth/index.ts
@@ -0,0 +1,61 @@
+import { Express, urlencoded, json } from 'express';
+import session, { SessionData } from 'express-session';
+import passport from 'passport';
+import { uid } from 'uid/secure';
+
+import { IAuthSessionService } from '../../services';
+
+import { AuthSessionStore } from './auth-session-store';
+
+declare global {
+    // eslint-disable-next-line @typescript-eslint/no-namespace
+    namespace Express {
+        // noinspection JSUnusedGlobalSymbols
+        interface User {
+            authIdentity: {
+                provider: 'google';
+                profileId: string;
+            };
+        }
+    }
+}
+
+interface WithPassportAuth {
+    app: Express;
+    authSessionService: IAuthSessionService<SessionData>;
+    cookieSecret: string;
+    productKey: string;
+}
+
+passport.serializeUser<Express.User>((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser<Express.User>((user, done) => {
+    done(null, user);
+});
+
+const withPassportAuth = async ({
+    app,
+    authSessionService,
+    cookieSecret,
+    productKey
+}: WithPassportAuth) => {
+    const store = new AuthSessionStore(authSessionService);
+
+    app.use(
+        json(),
+        urlencoded({ extended: true }),
+        session({
+            genid: () => uid(32),
+            name: `labset_${productKey}`,
+            secret: cookieSecret,
+            store,
+            resave: false,
+            saveUninitialized: true,
+            cookie: { secure: true, sameSite: 'none' }
+        })
+    );
+};
+
+export { withPassportAuth };
